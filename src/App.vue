23e0b7b73c174cb51d8cf9de56b4a9be93cf9164
Arsenuk <illya.arsenyuk@kpk-lp.com.ua>
2025-11-02 17:22:49 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <v-app>
+    <Header />
+  </v-app>  
+ </template>
+
+<script>
+import Header from './components/Header.vue';
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
